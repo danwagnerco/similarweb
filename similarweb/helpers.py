1c7cf6e90c76f6a3f78a60752747b3cdcf3d4ca6
Dan Wagner <danwagnerco@gmail.com>
2015-07-01 21:23:20 -0500

    DRY up design by extracting http get and handling

--- similarweb/helpers.py
+++ similarweb/helpers.py
@@ -1,8 +1,30 @@
+import requests
+try:
+    import simplejson as json
+except (ImportError, SyntaxError):
+    import json
+
 BAD_API_KEY = {"Error": "user_key_invalid"}
 BAD_URL = {"Error": "Malformed or Unknown URL"}
 BAD_DATE_ORDER = {"Error": "Date range is not valid"}
 BAD_UNKNOWN_ERROR = {"Error": "Unknown Error"}
 BAD_APP_STORE = {"Error": "App store must be 'apple' or 'google'"}
+
+def get_http_response(url):
+    try:
+        response = requests.get(url)
+        return json.loads(response.text)
+    except ValueError:
+        return BAD_URL
+
+
+def dictify(list_of_dicts, to_be_keys, to_be_values, stringify_keys = False):
+    values = [d[to_be_values] for d in list_of_dicts]
+    if stringify_keys:
+        keys = [str(d[to_be_keys]) for d in list_of_dicts]
+    else:
+        keys = [d[to_be_keys] for d in list_of_dicts]
+    return dict(zip(keys, values))
 
 
 def bad_inputs_to_traffic_or_sources_api(dictionary):
